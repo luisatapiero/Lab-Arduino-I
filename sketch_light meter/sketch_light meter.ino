4795ea32b46d16c26514a911f388efe5d3ddeb82
luisatapiero <79235499+luisatapiero@users.noreply.github.com>
2022-10-06 16:53:54 -0500

    First commit

---
+++ sketch_light meter/sketch_light meter.ino
@@ -0,0 +1,45 @@
+int pinLED1 = 8;
+int pinLED2 = 9;
+int pinLED3 = 10;
+int pinLED4 = 11;
+
+int foto = A3;
+
+int valorFoto = 0;
+
+void setup()
+{
+  pinMode(pinLED1, OUTPUT);
+  pinMode(pinLED2, OUTPUT);
+  pinMode(pinLED3, OUTPUT);
+  pinMode(pinLED4, OUTPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(pinLED1, LOW);
+  digitalWrite(pinLED2, LOW);
+  digitalWrite(pinLED3, LOW);
+  digitalWrite(pinLED4, LOW);
+  
+  valorFoto = analogRead(foto);
+  Serial.println(valorFoto);
+  
+  if (valorFoto > 250){
+    digitalWrite(pinLED1, HIGH);
+   } 
+  if (valorFoto > 400){
+    digitalWrite(pinLED2, HIGH);
+   } 
+  if (valorFoto > 650){
+    digitalWrite(pinLED3, HIGH);
+   } 
+  if (valorFoto > 800){
+    digitalWrite(pinLED4, HIGH);
+   } 
+  
+  delay(200);
+  
+}
